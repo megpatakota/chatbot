527073ee25484371da9d88e4c6ffc9d757d27988
Meghana Patakota <megpatakota@192.168.1.107>
2025-03-30 17:52:40 +0100

    manage.py

--- manage.py
+++ manage.py
@@ -3,11 +3,9 @@
 import os
 import sys
 from dotenv import load_dotenv
-from django.shortcuts import render
-from django.http import JsonResponse
-from litellm import completion  # Importing Litellm's completion function
+
+# Load environment variables from .env file
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 def main():
     """Run administrative tasks."""
@@ -21,19 +19,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-def home(request):
-    if request.method == "POST":
-        user_input = request.POST.get("message", "")
-        if user_input:
-            # Use Litellm's completion function to get a response
-            response = completion(
-                model="gpt-4o-mini",  # Gemini model = gemini/gemini-1.5-pro
-                messages=[{"role": "user", "content": "Hello, how are you?"}],
-            )
-            return response.choices[0].message.content
-    return render(request, "megbot/chat.html")
 
 
 if __name__ == "__main__":
