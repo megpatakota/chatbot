2b5c06f7f90b770a48c47dcde64b0a6d7f30167f
Meghana Patakota <megpatakota@192.168.1.107>
2025-03-30 15:07:04 +0100

    chatbot v1

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d*qvgurotqrx!^ozommrezpg^%94ye0*&*4ua99f5oa^1oro$e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
